68ffa74855cf3a1252ecb7588d9dde7a091900f5
otcova <otgercomas@gmail.com>
2022-08-27 09:56:29 +0200

    memory: grow with screen axis

--- memory/main.js
+++ memory/main.js
@@ -3,15 +3,23 @@ let level = 0;
 
 let state = "pauseclick"
 
-let w = 3;
-let h = 3;
+let board_width = 3;
+let board_height = 3;
 
-let matrixReal;
-let matrixRealCount;
-let matrix;
-let matrixCount;
+let boardSquares;
+let goodSquaresCount;
+let clickedGoodSquaresCount;
 
 let errorCount = 0;
+
+const size = () => {
+    let w = board_width, h = board_height;
+    if (width > height) {
+        w = board_height;
+        h = board_width;
+    }
+    return min(width / w, height / (h + 1.3)) - 10
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -21,77 +29,64 @@ function setup() {
 function draw() {
     background(48, 48, 100);
 
-    translate(width / 2, height * 0.6);
-    scale(min(width / w, height / (h + 1.3)) - 10);
     noStroke();
     fill(255);
 
-    textSize(0.8);
-    textAlign(CENTER, CENTER);
-    text(level, 0, -h / 2 - 0.65);
+    drawScore();
 
-    for (let x = 0; x < w; x++) {
-        for (let y = 0; y < h; y++) {
-            if (state == "mostrar") {
-                if (matrixReal[x][y]) fill(255);
-                else fill(100, 100, 120);
-            } else if (state == "recordar" || state == "win") {
-                if (matrix[x][y] == "error") fill(255, 100, 100);
-                else if (matrix[x][y]) fill(255);
-                else fill(100, 100, 120);
-            } else if (state == "gameover") {
-                if (matrix[x][y] == "error") fill(255, 100, 100);
-                else if (matrix[x][y]) fill(255);
-                else if (matrixReal[x][y]) fill(100, 230, 100);
-                else fill(100, 100, 120);
-            } else if (state == "pause" || state == "pauseclick") {
-                fill(100, 100, 120);
-            }
-            rect(x + 0.05 - w / 2, y + 0.05 - h / 2, 0.9, 0.9, 0.2);
+    for (let x = 0; x < board_width; x++) {
+        for (let y = 0; y < board_height; y++) {
+            const square = boardSquares[x][y];
+            if (square.error) fill(255, 100, 100);
+            else if (square.clicked) fill(255);
+            else if (square.good && state == "mostrar") fill(255);
+            else if (square.good && state == "gameover") fill(100, 230, 100);
+            else fill(100, 100, 120);
+            rect(...squareRect(x, y, .1, .2));
         }
     }
+
+}
+
+function drawScore() {
+    textSize(0.8 * size());
+    textAlign(CENTER, CENTER);
+    text(
+        level,
+        width / 2,
+        height * 0.6 - (0.6 + (width < height ? board_height / 2 : board_width / 2)) * size()
+    );
 }
 
 function newLevel() {
     errorCount = 0;
-    matrixRealCount = 0;
-    matrixCount = 0;
+    goodSquaresCount = 0;
+    clickedGoodSquaresCount = 0;
     level += 1;
 
-    if (level % 5 == 0) w++;
-    else if ((level - 3) % 4 == 0) h++;
+    if (level % 5 == 0) board_width++;
+    else if (level % 2 == 0) board_height++;
 
-    matrixReal = [];
-    matrix = [];
-    for (let x = 0; x < w; x++) {
-        matrixReal.push([]);
-        matrix.push([]);
-        for (let y = 0; y < h; y++) {
-            matrixReal[x].push(false);
-            matrix[x].push(false);
-        }
+    boardSquares = [];
+    for (let x = 0; x < board_width; x++) {
+        boardSquares.push(Array(board_height).fill(0).map(() => ({})));
     }
 
     for (let i = 0; i < random(level + 2, 1.3 * level + 3); i++) {
-        matrixReal[parseInt(random(0, w))][parseInt(random(0, h))] = true;
-    }
-
-    for (let x = 0; x < w; x++) {
-        for (let y = 0; y < h; y++) {
-            if (matrixReal[x][y]) matrixRealCount++;
+        const x = floor(random(0, board_width)), y = floor(random(0, board_height));
+        if (!boardSquares[x][y].good) {
+            boardSquares[x][y] = { good: true };
+            goodSquaresCount++;
         }
     }
+
     if (level == 1) {
-        state = "pauseclick"
+        state = "pauseclick";
     }
     else {
         state = "pause";
-        setTimeout(function () {
-            state = "mostrar";
-        }, 1000);
-        setTimeout(function () {
-            state = "recordar";
-        }, 2500);
+        setTimeout(() => state = "mostrar", 1000);
+        setTimeout(() => state = "recordar", 2500);
     }
 }
 
@@ -117,40 +112,34 @@ function touchStarted() {
 
 function mousePressed() {
 
+    let w = board_width, h = board_height;
+
     if (state == "gameover") {
         level = 0;
-        w = 3;
-        h = 3;
+        board_width = 3;
+        board_height = 3;
         newLevel();
     }
     else if (state == "pauseclick") {
-        setTimeout(function () {
-            state = "mostrar";
-        }, 300);
-        setTimeout(function () {
-            state = "recordar";
-        }, 1800);
+        setTimeout(() => state = "mostrar", 300);
+        setTimeout(() => state = "recordar", 1800);
     }
     else if (state == "recordar") {
 
-        let tx = width / 2;
-        let ty = height * 0.6;
-        let s = min(width / w, height / (h + 1.3)) - 10;
-
         for (let x = 0; x < w; x++) {
             for (let y = 0; y < h; y++) {
-                if (overRect(tx + s * (x + 0.2 - w / 2), ty + s * (y + 0.2 - h / 2), s * 0.6, s * 0.6)) {
-                    if (matrixReal[x][y]) {
-                        if (matrix[x][y] != true) {
-                            matrix[x][y] = true;
-                            matrixCount++;
-                            if (matrixCount == matrixRealCount) {
+                if (overRect(...squareRect(x, y, .3))) {
+                    if (boardSquares[x][y].good) {
+                        if (!boardSquares[x][y].clicked) {
+                            boardSquares[x][y].clicked = true;
+                            clickedGoodSquaresCount++;
+                            if (clickedGoodSquaresCount == goodSquaresCount) {
                                 state = "win"
                                 setTimeout(newLevel, 1000);
                             }
                         }
-                    } else if (matrix[x][y] != "error") {
-                        matrix[x][y] = "error";
+                    } else if (!boardSquares[x][y].error) {
+                        boardSquares[x][y].error = true;
                         errorCount++;
                         if (errorCount >= 2) {
                             state = "gameover"
@@ -174,6 +163,28 @@ function overRect(x, y, width, height) {
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
-document.oncontextmenu = function () {
-    return false;
+document.oncontextmenu = () => false;
+
+function squareRect(x, y, margin, round) {
+    let w = board_width, h = board_height;
+    if (width > height) {
+        w = board_height;
+        h = board_width;
+        let temp_x = x;
+        x = y;
+        y = temp_x;
+    }
+
+    const offset_x = width / 2;
+    const offset_y = height * 0.6;
+    
+    const rect = [
+        offset_x + size() * (x + margin / 2 - w / 2),
+        offset_y + size() * (y + margin / 2 - h / 2),
+        size() * (1 - margin),
+        size() * (1 - margin)
+    ];
+
+    if (round) return [...rect, size() * round];
+    return rect;
 }
