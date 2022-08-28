60076dd52717aacb12d00207c697362917094c23
otcova <otgercomas@gmail.com>
2022-08-28 12:43:16 +0200

    multi touch

--- memory/main.js
+++ memory/main.js
@@ -1,146 +1,150 @@
 
-let level = 0;
+let level = 0
 
 let state = "pauseclick"
 
-let board_width = 3;
-let board_height = 3;
+let board_width = 3
+let board_height = 3
 
-let boardSquares;
-let goodSquaresCount;
-let clickedGoodSquaresCount;
+let boardSquares
+let goodSquaresCount
+let clickedGoodSquaresCount
 
-let errorCount = 0;
+let errorCount = 0
 
 const size = () => {
-    let w = board_width, h = board_height;
-    if (width > height) {
-        w = board_height;
-        h = board_width;
-    }
-    return min(width / (w + .5), height / (h + 1.5))
+    if (width > height)
+        return min(width / (board_height + .5), height / (board_width + 1.5))
+    return min(width / (board_width + .5), height / (board_height + 1.5))
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    newLevel();
+    createCanvas(windowWidth, windowHeight)
+    newLevel()
 }
 
 function draw() {
-    background(48, 48, 100);
+    background(48, 48, 100)
 
-    noStroke();
-    fill(255);
+    noStroke()
+    fill(255)
 
-    drawScore();
+    drawScore()
 
     for (let x = 0; x < board_width; x++) {
         for (let y = 0; y < board_height; y++) {
-            const square = boardSquares[x][y];
-            if (square.error) fill(255, 100, 100);
-            else if (square.clicked) fill(255);
-            else if (square.good && state == "mostrar") fill(255);
-            else if (square.good && state == "gameover") fill(100, 230, 100);
-            else fill(100, 100, 120);
-            rect(...squareRect(x, y, .1, .2));
+            const square = boardSquares[x][y]
+            if (square.error) fill(255, 100, 100)
+            else if (square.clicked) fill(255)
+            else if (square.good && state == "mostrar") fill(255)
+            else if (square.good && state == "gameover") fill(100, 230, 100)
+            else fill(100, 100, 120)
+            rect(...squareRect(x, y, .1, .2))
         }
     }
-
 }
 
 function drawScore() {
-    textSize(0.8 * size());
-    textAlign(CENTER, CENTER);
+    textSize(0.8 * size())
+    textAlign(CENTER, CENTER)
     text(
         level,
         width / 2,
         (height + size()) / 2 - (0.6 + (width < height ? board_height / 2 : board_width / 2)) * size()
-    );
+    )
 }
 
 function newLevel() {
-    errorCount = 0;
-    goodSquaresCount = 0;
-    clickedGoodSquaresCount = 0;
-    level += 1;
+    errorCount = 0
+    goodSquaresCount = 0
+    clickedGoodSquaresCount = 0
+    level += 1
 
-    if (level % 5 == 0) board_width++;
-    else if (level % 2 == 0) board_height++;
+    if (level % 5 == 0) board_width++
+    else if (level % 2 == 0) board_height++
 
-    boardSquares = [];
+    boardSquares = []
     for (let x = 0; x < board_width; x++) {
-        boardSquares.push(Array(board_height).fill(0).map(() => ({})));
+        boardSquares.push(Array(board_height).fill(0).map(() => ({})))
     }
-    
+
     for (let i = 0; i < board_width * board_height / 2; i++) {
-        const x = floor(random(0, board_width)), y = floor(random(0, board_height));
+        const x = floor(random(0, board_width)), y = floor(random(0, board_height))
         if (!boardSquares[x][y].good) {
-            boardSquares[x][y] = { good: true };
-            goodSquaresCount++;
+            boardSquares[x][y] = { good: true }
+            goodSquaresCount++
         }
     }
 
     if (level == 1) {
-        state = "pauseclick";
+        state = "pauseclick"
     }
     else {
-        state = "pause";
-        setTimeout(() => state = "mostrar", 1000);
-        setTimeout(() => state = "recordar", 2500);
+        state = "pause"
+        setTimeout(() => state = "mostrar", 1000)
+        setTimeout(() => state = "recordar", 2500)
     }
 }
 
 function mouseDragged() {
     if (state != "gameover")
-        mousePressed();
+        mousePressed()
+}
+
+function touchMoved() {
+    if (state != "gameover") {
+        console.log(touches)
+        for (const touch of touches) {
+            mouseX = touch.x
+            mouseY = touch.y
+            mousePressed()
+        }
+    }
 }
 
 function touchStarted() {
     if (!fullscreen()) {
-        fullscreen(true);
+        fullscreen(true)
         setTimeout(function () {
-            fullscreen(true);
-        }, 100);
+            fullscreen(true)
+        }, 100)
     } else {
-        mouseX = touches[touches.length - 1].x;
-        mouseY = touches[touches.length - 1].y;
-        mousePressed();
+        mouseX = touches[touches.length - 1].x
+        mouseY = touches[touches.length - 1].y
+        mousePressed()
     }
-    return false;
+    return false
 }
 
 
 function mousePressed() {
-
-    let w = board_width, h = board_height;
-
     if (state == "gameover") {
-        level = 0;
-        board_width = 3;
-        board_height = 3;
-        newLevel();
+        level = 0
+        board_width = 3
+        board_height = 3
+        newLevel()
     }
     else if (state == "pauseclick") {
-        setTimeout(() => state = "mostrar", 300);
-        setTimeout(() => state = "recordar", 1800);
+        setTimeout(() => state = "mostrar", 300)
+        setTimeout(() => state = "recordar", 1800)
     }
     else if (state == "recordar") {
 
-        for (let x = 0; x < w; x++) {
-            for (let y = 0; y < h; y++) {
+        for (let x = 0; x < board_width; x++) {
+            for (let y = 0; y < board_height; y++) {
                 if (overRect(...squareRect(x, y, .3))) {
                     if (boardSquares[x][y].good) {
                         if (!boardSquares[x][y].clicked) {
-                            boardSquares[x][y].clicked = true;
-                            clickedGoodSquaresCount++;
+                            boardSquares[x][y].clicked = true
+                            clickedGoodSquaresCount++
                             if (clickedGoodSquaresCount == goodSquaresCount) {
                                 state = "win"
-                                setTimeout(newLevel, 1000);
+                                setTimeout(newLevel, 1000)
                             }
                         }
                     } else if (!boardSquares[x][y].error) {
-                        boardSquares[x][y].error = true;
-                        errorCount++;
+                        boardSquares[x][y].error = true
+                        errorCount++
                         if (errorCount >= 2) {
                             state = "gameover"
                         }
@@ -154,37 +158,37 @@ function mousePressed() {
 function overRect(x, y, width, height) {
     if (mouseX >= x && mouseX <= x + width &&
         mouseY >= y && mouseY <= y + height) {
-        return true;
+        return true
     } else {
-        return false;
+        return false
     }
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight)
 }
-document.oncontextmenu = () => false;
+document.oncontextmenu = () => false
 
 function squareRect(x, y, margin, round) {
-    let w = board_width, h = board_height;
+    let w = board_width, h = board_height
     if (width > height) {
-        w = board_height;
-        h = board_width;
-        let temp_x = x;
-        x = y;
-        y = temp_x;
+        w = board_height
+        h = board_width
+        let temp_x = x
+        x = y
+        y = temp_x
     }
 
-    const offset_x = width / 2;
-    const offset_y = (height + size()) / 2;
+    const offset_x = width / 2
+    const offset_y = (height + size()) / 2
 
     const rect = [
         offset_x + size() * (x + margin / 2 - w / 2),
         offset_y + size() * (y + margin / 2 - h / 2),
         size() * (1 - margin),
         size() * (1 - margin)
-    ];
+    ]
 
-    if (round) return [...rect, size() * round];
-    return rect;
+    if (round) return [...rect, size() * round]
+    return rect
 }
